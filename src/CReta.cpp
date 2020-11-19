4e82b4118c06206bcdbdd783de0ed43c88bf014a
hereisjohnny2 <joaomarceloccarvalho@gmail.com>
2020-11-19 15:48:20 -0300

    modifications on CReta class

--- src/CReta.cpp
+++ src/CReta.cpp
@@ -43,6 +43,28 @@ void CReta::Saida(std::string nomeArquivo)
     fout.close();
 }
 
+std::ostream &operator<<(std::ostream &os, const CReta &reta)
+{
+    os << reta.a << "\t" << reta.b;
+    return os;
+}
+
+std::istream &operator<<(std::istream &in, CReta &reta)
+{
+    in >> reta.a >> reta.b;
+    return in;
+}
+
+bool operator==(const CReta &reta1, const CReta &reta2)
+{
+    return (reta1.a == reta2.a and reta1.b == reta2.b);
+}
+
+bool operator!=(const CReta &reta1, const CReta &reta2)
+{
+    return !(reta1 == reta2);
+}
+
 void CReta::Plot(CGnuplot &grafico) const
 {
     std::vector<double> v1{0.0, Fx(0.0)};
@@ -55,4 +77,10 @@ void CReta::Plot(CGnuplot &grafico) const
     std::cin.clear();                              // Zera estado de cin
     std::cin.ignore(std::cin.rdbuf()->in_avail()); // Ignora
     std::cin.get();
+}
+
+CGnuplot &operator<<(CGnuplot &grafico, const CReta &reta)
+{
+    reta.Plot(grafico);
+    return grafico;
 }
